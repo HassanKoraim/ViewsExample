9fe89b0d138fa115babb494020a6e4bef8b4a4fc
Hassan koraim <Hassankoraim2@gmail.com>
2024-03-31 01:07:53 +0200

    ViewData"Part1"

--- ViewsExample/Controllers/HomeController.cs
+++ ViewsExample/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ViewsExample.Models;
 
 namespace ViewsExample.Controllers
 {
@@ -8,6 +9,14 @@ namespace ViewsExample.Controllers
         [Route("/")]
         public IActionResult Index()
         {
+            ViewData["appTitle"] = "Index Title";
+            List<Person> people = new List<Person>()
+            {
+               new Person() { Name = "Hassan", BirthDate = Convert.ToDateTime("2010-02-08"), PersonGender = Gender.Male},
+               new Person() { Name = "Ali", BirthDate = Convert.ToDateTime("2000-07-08") , PersonGender = Gender.Female},
+               new Person() { Name = "Ahmed", BirthDate = Convert.ToDateTime("1999-02-05"), PersonGender = Gender.Other }
+            };
+            ViewData["people"] = people;
             /*
              * by default, it will look for a view named Index,
                in a folder named Views then subfolder named Home
