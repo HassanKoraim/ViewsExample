9e3d965b7364ca7ea3ee386b0aa77b7be7497e82
Hassan koraim <Hassankoraim2@gmail.com>
2024-04-03 00:10:31 +0200

    92. Strongly Typed Views with Multiple Models

--- ViewsExample/Controllers/HomeController.cs
+++ ViewsExample/Controllers/HomeController.cs
@@ -21,7 +21,44 @@ namespace ViewsExample.Controllers
              * by default, it will look for a view named Index,
                in a folder named Views then subfolder named Home
              */
-            return View(); 
+            return View(people); 
+        }
+        [Route("person-details/{name}")]
+        public IActionResult Details(string name)
+        {
+            if(name == null)
+            {
+                return Content("Person name can't be null");
+            }
+            List<Person>? people = new List<Person>()
+            {
+               new Person() { Name = "Hassan", BirthDate = Convert.ToDateTime("2010-02-08"), PersonGender = Gender.Male},
+               new Person() { Name = "Ali", BirthDate = Convert.ToDateTime("2000-07-08") , PersonGender = Gender.Female},
+               new Person() { Name = "Ahmed", BirthDate = Convert.ToDateTime("1999-02-05"), PersonGender = Gender.Other }
+            };
+            Person? matchingPerson = people.Where(temp => temp.Name == name).FirstOrDefault();
+            return View(matchingPerson);
+        }
+        [Route("person-with-products")]
+        public IActionResult PersonWithProudcts()
+        {
+            Person person = new Person()
+            {
+                Name = "Hassan",
+                BirthDate = Convert.ToDateTime("2010-02-08"),
+                PersonGender = Gender.Male
+            };
+            Product product = new Product()
+            {
+                ProductId = 1,
+                ProductName = "Laptop"
+            };
+            PersonAndProductWrapperClass personAndProductWrapperClass = new PersonAndProductWrapperClass()
+            {
+                PersonData = person,
+                ProductData = product
+            };
+            return View("PersonAndProduct", personAndProductWrapperClass);
         }
     }
 }
---
+++ ViewsExample/Models/PersonAndProductWrapperClass.cs
@@ -0,0 +1,8 @@
+﻿namespace ViewsExample.Models
+{
+    public class PersonAndProductWrapperClass
+    {
+        public Person? PersonData { get; set; }
+        public Product? ProductData { get; set; }
+    }
+}
